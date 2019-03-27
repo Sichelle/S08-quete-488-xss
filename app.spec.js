21ade4d583e7b8738f220694a293eefeac66931a
Sichelle <sichelle.chiffert@gmail.com>
2019-03-27 17:24:22 +0100

    first commit

---
+++ app.spec.js
@@ -0,0 +1,67 @@
+const request = require("supertest");
+const dataInterface = require("./data-interface");
+
+const app = require("./app");
+const agent = request.agent(app);
+
+describe("app", () => {
+  describe("when authenticated", () => {
+    beforeEach(async () => {
+      await agent
+        .post("/login")
+        .send("username=randombrandon&password=randompassword");
+    });
+
+    describe("POST /messages", () => {
+      describe("with non-empty content", () => {
+        describe("with JavaScript in personalWebsiteURL", () => {
+          it("responds with error", async done => {
+            await agent
+              .post("/messages")
+              .send(
+                "content=Hola&personalWebsiteURL=javascript:alert('Hacked!');"
+              )
+              .expect("Content-Type", "application/json; charset=utf-8")
+              .expect(400);
+            done();
+          });
+          it("does not callcreateMessage function", async done => {
+            const mockFunc = (dataInterface.createMessage = jest.fn());
+            await agent
+              .post("/messages")
+              .send(
+                "content=Hola&personalWebsiteURL=javascript:alert('Hacked!');"
+              )
+              .expect(400);
+            expect(mockFunc).toHaveBeenCalledTimes(0);
+            done();
+          });
+        });
+
+        describe("with HTTP URL in personalWebsiteURL", () => {
+          it("responds with success", async done => {
+            await agent
+              .post("/messages")
+              .send(
+                "content=Hola&personalWebsiteURL=https://giphy.com/stories/pandamonium-0cab7f0b-e7f5"
+              )
+              .expect("Content-Type", "application/json; charset=utf-8")
+              .expect(201);
+            done();
+          });
+          it("calls createMessage function", async done => {
+            const mockFunc = (dataInterface.createMessage = jest.fn());
+            await agent
+              .post("/messages")
+              .send(
+                "content=Hola&personalWebsiteURL=https://giphy.com/stories/pandamonium-0cab7f0b-e7f5"
+              )
+              .expect(201);
+            expect(mockFunc).toHaveBeenCalledTimes(1);
+            done();
+          });
+        });
+      });
+    });
+  });
+});
